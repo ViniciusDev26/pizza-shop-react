4f75aafcd2d4bebb7ea50fa7d648a356c7ad72ba
Carlos Vinicius <viniciusdev.26@gmail.com>
2024-01-14 01:25:34 -0300

    feat: create login page

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,36 @@
 import { Helmet } from 'react-helmet-async'
 
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
 export const SignIn = () => {
   return (
     <>
       <Helmet title="Sign in" />
-      <h1>Sign In</h1>
+      <div className="p-8">
+        <div className="flex w-[360px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Acessar Painel
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Acompanhe suas vendas pelo painel do parceiro
+            </p>
+          </div>
+
+          <form className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Email</Label>
+              <Input id="email" type="email" />
+            </div>
+
+            <Button className="w-full" type="submit">
+              Acessar Painel
+            </Button>
+          </form>
+        </div>
+      </div>
     </>
   )
 }
